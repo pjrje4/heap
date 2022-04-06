1a2fff0c6d6a808f42340a054fcb69250d6e5f7d
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-04-05 18:31:11 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,23 +9,27 @@ int main() {
 	Heap* h = new Heap(100);
 	char input[5000];
 	cout << "(FILE) or Command Line (INPUT)" << endl;
-	cin >> input;
+	cin.getline(input, 10);
 	if (strcmp(input, "INPUT") == 0) {
 		cout << "Enter numbers separated by spaces:" << endl;
-		int count = 0;
+		cin.getline(input, 5000);
+		fstream iFile;
+		iFile.open("thisisthestupidestwaytodothisbutimsickandlazy.txt", ios::out | ios::trunc);
+		if (!iFile) {
+			cout << "Can't find thisisthestupidestwaytodothisbutimsickandlazy.txt.  Make sure you download it from my repo." << endl;
+			return 0;
+		}
+		else {
+			iFile << input;
+			iFile.close();
+		}
+		iFile.open("thisisthestupidestwaytodothisbutimsickandlazy.txt", ios::in);
 		int number = 0;
-		while (cin >> number) {
-			cout << number << endl;
+		while (iFile >> number) {
 			if (number != 0) {
 				h->addNum(number);
 			}
-			/*count++;
-			if (count <= 100) {
-				break;
-			}*/
-			cout << "count: " << count << endl;
 		}
-
 	}
 	if (strcmp(input, "FILE") == 0) {
 		fstream iFile;

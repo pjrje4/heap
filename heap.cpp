f8e228c72cb0989690b684e3a48d2a5b7ecd5ed4
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-04-05 18:41:04 -0700

    Add files via upload

--- heap.cpp
+++ heap.cpp
@@ -22,80 +22,79 @@ Heap::Heap(int size) { // parametized constructor
 }
 Heap::~Heap() { // destructor
 }
-//getters and setters
-void Heap::debug() {
+void Heap::debug() { // to debug the array
 	cout << endl;
 	for (int i = 0; i < maxSize; i++) {
 		cout << data[i] << ", ";
 	}
 	cout << "debug" << endl;
 }
-int Heap::left(int index) {
+int Heap::left(int index) { // left
 	return index*2;
 }
-int Heap::right(int index) {
+int Heap::right(int index) { // right
 	return index*2+1;
 }
-int Heap::parent(int index) {
+int Heap::parent(int index) { // parent of index
 	return index/2;
 }
-void Heap::swap(int x, int y) {
+void Heap::swap(int x, int y) { // swap 2 index
 	int temp = data[x];
 	data[x] = data[y];
 	data[y] = temp;
 }
 
-void Heap::addNum(int num) {
+void Heap::addNum(int num) { // add a number
 	int i = 1;
 	while (data[i] != 0) {
 		i++;
 	}
 	data[i] = num;
-	swim(i);
+	swim(i); // make sure not broken
 	currentSize++;
 	return;
 }
-void Heap::out() {
+void Heap::out() { // output whole thing
 	while (currentSize > 0) {
 		cout << data[1] << ", ";
 		data[1] = data[currentSize];
 		data[currentSize] = 0;
 		currentSize--;
-		sink(1);
+		sink(1); // make sure not broken
 	}
 	return;
 }
-void Heap::print(int index) {
-	if (left(index) <= currentSize) {
+void Heap::print(int index) { // print using tabs visually
+	if (left(index) <= currentSize) { // recursive left
 		print(left(index));
 	}
 
-	int tabs = log10(index) / log10(2);
+	int tabs = log10(index) / log10(2); // how many tabs
 	for (int i = 0; i < tabs; i++) {
 		cout << "    ";
 	}
 	cout << data[index] << endl;
 
-	if (right(index) <= currentSize) {
+	if (right(index) <= currentSize) { // recursive right
 		print(right(index));
 	}
 
 	return;
 }
-void Heap::sink(int index) {
-	if (data[left(index)] == 0 || left(index) > currentSize) {
+void Heap::sink(int index) { // sink down
+	if (data[left(index)] == 0 || left(index) > currentSize) { // out of bounds
 		return;
-	}
-	if (data[index] < data[left(index)]) {
+	} 
+	if (data[index] < data[left(index)]) { // left greater then
 		swap(index, left(index));
 		sink(index);
 		sink(left(index));
 		return;
 	}
-	if (data[right(index)] == 0 || right(index) > currentSize) {
+	if (data[right(index)] == 0 || right(index) > currentSize) { // out of bounds
 		return;
 	}
-	if (data[index] < data[right(index)]) {
+	if (data[index] < data[right(index)]) { //right greater then
 		swap(index, right(index));
 		sink(index);
 		sink(right(index));
@@ -103,11 +102,11 @@ void Heap::sink(int index) {
 	}
 	return;
 }
-void Heap::swim(int index) {
-	if (index == 1) {
+void Heap::swim(int index) { // sort up
+	if (index == 1) { // root
 		return;
 	}
-	if (data[parent(index)] < data[index]) {
+	if (data[parent(index)] < data[index]) { // if parent greater then swap
 		swap(parent(index), index);
 		swim(parent(index));
 	}
--- main.cpp
+++ main.cpp
@@ -3,19 +3,27 @@
 #include <cstring>
 #include <fstream>
 
+#if 0
+Justin Iness
+4/5/2022
+C++ Programming
+Heap
+#endif
+
 using namespace std;
 
 int main() {
-	Heap* h = new Heap(100);
+	Heap* h = new Heap(100); // new heap size 100 per requirements
 	char input[5000];
 	cout << "(FILE) or Command Line (INPUT)" << endl;
 	cin.getline(input, 10);
-	if (strcmp(input, "INPUT") == 0) {
+	if (strcmp(input, "INPUT") == 0) { // input through command line
 		cout << "Enter numbers separated by spaces:" << endl;
 		cin.getline(input, 5000);
+		//make a new file and write all the numbers to it
 		fstream iFile;
 		iFile.open("thisisthestupidestwaytodothisbutimsickandlazy.txt", ios::out | ios::trunc);
-		if (!iFile) {
+		if (!iFile) { 
 			cout << "Can't find thisisthestupidestwaytodothisbutimsickandlazy.txt.  Make sure you download it from my repo." << endl;
 			return 0;
 		}
@@ -23,11 +31,14 @@ int main() {
 			iFile << input;
 			iFile.close();
 		}
+		// then read the numbers from it using the >> operator
+		// I did this whole thing because I wanted to read in my numbers using >>
+		// help me
 		iFile.open("thisisthestupidestwaytodothisbutimsickandlazy.txt", ios::in);
 		int number = 0;
 		while (iFile >> number) {
 			if (number != 0) {
-				h->addNum(number);
+				h->addNum(number); // add to heap
 			}
 		}
 	}
@@ -35,6 +46,7 @@ int main() {
 		fstream iFile;
 		cout << "What is the file's name?" << endl;
 		cin >> input;
+		// open file and read numbers from it
 		iFile.open(input, ios::in);
 		if (!iFile) {
 			cout << "Can't find " << input << ".  Try including .txt?" << endl;
@@ -45,12 +57,12 @@ int main() {
 			int number = 0;
 			while (iFile >> number) {
 				if (number != 0) {
-					h->addNum(number);
+					h->addNum(number); // add to heap
 				}
 			}
 		}	
 	}
-	h->print(1);
-	h->out();
+	h->print(1); // print heap from root
+	h->out(); // output least to greatest
 	return 0;
 }
